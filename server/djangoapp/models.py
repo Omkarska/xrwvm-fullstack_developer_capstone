5baca1b4fb42fb6f0f0d444fbd94315d85bca498
Omkar Kanase <omkarkanase@outlook.com>
2024-09-19 15:33:34 +0530

    bug-fix

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -15,9 +15,8 @@ class CarMake(models.Model):
     def __str__(self):
         return self.name  # Return the name as the string representation
 
-
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    car_make=models.ForeignKey(CarMake,on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -32,6 +31,5 @@ class CarModel(models.Model):
             MinValueValidator(2015)
         ])
     # Other fields as needed
-
     def __str__(self):
-        return self.name  # Return the name as the string representation
+        return self.name 
